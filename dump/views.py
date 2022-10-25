5426611cd739e8c6e8deee1416f4bcb0b7452d5f
Kelvinkaranja <116677736+Kelvinkaranja@users.noreply.github.com>
2022-10-26 01:20:29 +0300

    Add files via upload

---
+++ dump/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .forms import Registrationform
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Login, Contents
+# Create your views here.
+def home(response):
+	return render(response,'dump/top.html',{})
+
+def login(response):
+	return render(response,'dump/list.html',{})
+
+def registration(response):
+	if response.POST:
+		form=Registrationform(response.POST)
+		if form.is_valid():
+			form.save()
+		return HttpResponseRedirect('dump/welcome.html',{'user':username})
+	return render(response,'dump/regform.html',{"form":Registrationform})
+
+def welcome(response):
+	form=Registrationform(response.POST)
+	form.save()
+	username = form.cleaned_data.get("username")
+	usernames=Login.objects.all()
+	for a in usernames:
+		user=(a.username)
+		users=user.upper()
+	log=Login(username)
+	return render(response, 'dump/welcome.html',{'user':users})
+
